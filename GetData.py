3e040c29a82c6c1c64beb1b2c235cbef0968c848
Arnav Gupta <arnavbrookvale@gmail.com>
2018-01-13 19:13:08 -0800

    Update GetData.py

--- GetData.py
+++ GetData.py
@@ -18,13 +18,7 @@ def record_game():
     os.system('rm -rf GamePics/game{}'.format(n))
     os.system('mkdir GamePics/game{}'.format(n))
     i = 0
-    print 'Wait to start your game'
-    print 'Make sure to only use the up arrow key (no down or spacebar)'
-    time.sleep(0.5)
-    print 'If you lose before the night time or start very late, quickly'
-    print 'stop the program, restart, and use the same game number again'
-    time.sleep(0.5)
-    print'You may have to run with sudo'
+
     time.sleep(2.5)
     print 'GO!'
     s = time.time()
